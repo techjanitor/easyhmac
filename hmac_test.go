60dabdd47952e6d70880715f818fdb60a60fcf5d
techjanitor <puntme@gmail.com>
2015-04-30 00:26:38 -0500

    add test

---
+++ hmac_test.go
@@ -0,0 +1,28 @@
+package easyhmac
+
+import (
+	"github.com/stretchr/testify/assert"
+	"github.com/techjanitor/easyhmac"
+	"testing"
+)
+
+func TestEncode(t *testing.T) {
+	easyhmac.Secret = "test"
+
+	// Initialize SignedMessage struct with secret
+	key := easyhmac.SignedMessage{}
+
+	// Add payload data
+	key.Payload = "test message"
+
+	// Create HMAC signature
+	key.Sign()
+
+	// Marshal message to JSON and encode in url-safe base64
+	signedkey, err := key.Encode()
+
+	assert.NoError(t, err, "should be no error")
+
+	assert.Equal(t, "eyJwIjoiZEdWemRDQnRaWE56WVdkbCIsInMiOiJZbTVpYVUxUmRrNURkVEUzY0U4clFVRkVaV1Z5Wnk5bmFYZFZjV3RPTUVweVFYTlRkRWRpUVUxcVVUMD0ifQ==", signedkey, "they should be equal")
+
+}
